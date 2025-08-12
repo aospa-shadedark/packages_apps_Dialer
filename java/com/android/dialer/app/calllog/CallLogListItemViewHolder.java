4f0eb81f8f5ea5b162203b642e91b938ad0e3995
jendermine <jendermine@gmail.com>
2025-08-12 11:46:38 +0530

    Dialer: Rework in-call theme colors, statusbar and notification styling
    Change-Id: I9b19377bd4bdca03b3a9349409e836a689af04c8

--- java/com/android/dialer/app/calllog/CallLogListItemViewHolder.java
+++ java/com/android/dialer/app/calllog/CallLogListItemViewHolder.java
@@ -856,6 +856,29 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
       return;
     }
 
+    if (view.getId() == R.id.details_action) {
+      CallLogAsyncTaskUtil.getCallDetails(
+          context,
+          number,
+          (entries) -> {
+            if (entries != null) {
+              boolean canReportCallerId =
+                  cachedNumberLookupService != null
+                      && cachedNumberLookupService.canReportAsInvalid(
+                          info.sourceType, info.objectId);
+              final Intent intent =
+                  IntentProvider.getCallDetailIntentProvider(
+                          entries,
+                          buildContact(),
+                          canReportCallerId,
+                          canSupportAssistedDialing())
+                      .getClickIntent(context);
+              DialerUtils.startActivityWithErrorToast(context, intent);
+            }
+          });
+      return;
+    }
+
     final IntentProvider intentProvider = (IntentProvider) view.getTag();
     if (intentProvider == null) {
       return;
